aeddba7758bc6b688e87a6ec338299b6a167b949
vvcb <vvcb.n1@gmail.com>
2023-01-21 18:53:28 +0000

    Saving before refreshing line endings

--- init.bat
+++
@@ -1,3 +0,0 @@
-mamba env update -f ./environment.yml --prune && ^
-conda activate hdruk_aa && ^
-pip install -e .
--- init.sh
+++
@@ -1,7 +0,0 @@
-# Project initialisation Script.
-# This runs a series of commands to setup the entire project environment
-# Depends on a version of anaconda or miniconda installed with mamba and pip on PATH
-# If using this on Windows, run in an Anaconda Powershell Prompt
-
-mamba env update -f ./environment.yml --prune
-mamba activate hdruk_aa
