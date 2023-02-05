8dc40c0204dba0c9b4c4728d4c787d19af1d681e
vvcb <vvcb.n1@gmail.com>
2023-02-05 23:08:31 +0000

    Installable package with pyproject.toml
    Squashed commit of the following:
    
    commit d6469be175394eb50a76cf1a66212e714be4123b
    Author: vvcb <vvcb.n1@gmail.com>
    Date:   Sun Feb 5 22:51:45 2023 +0000
    
        Working pyproject.toml setup
    
    commit 523f5e0ba2851ca5027896510f66ac86f3507a23
    Author: vvcb <vvcb.n1@gmail.com>
    Date:   Sun Feb 5 21:52:36 2023 +0000
    
        Initial commit for pip installer

--- avoidable_admissions/__init__.py
+++ avoidable_admissions/__init__.py
@@ -1,3 +1,7 @@
+from xml.etree.ElementTree import VERSION
+
 from avoidable_admissions import data, features, models, utils, visualization
 
 __all__ = ["data", "features", "models", "utils", "visualization"]
+
+VERSION = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="avoidable_admissions",
-    packages=find_packages(),
-    version="0.1.0",
-    description="HDRUK Data Science Collaboration on Avoidable Admissions in the NHS",
-    author="LTHTR-DST",
-    license="MIT",
-)
+setup()
