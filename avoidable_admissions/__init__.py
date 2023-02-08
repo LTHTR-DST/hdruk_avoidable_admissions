239a4be54405099f6cc9407955e2d5f4624b5633
vvcb <vvcb.n1@gmail.com>
2023-02-08 17:39:49 +0000

    Remove automatically added import

--- avoidable_admissions/__init__.py
+++ avoidable_admissions/__init__.py
@@ -1,5 +1,3 @@
-from xml.etree.ElementTree import VERSION
-
 from avoidable_admissions import data, features, models, utils, visualization
 
 __all__ = ["data", "features", "models", "utils", "visualization"]
